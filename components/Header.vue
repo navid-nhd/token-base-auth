8d714971fcf8bbd88a697d24f431d42d8d869e52
Navid Nahardani <nahardaninavid1993@gmail.com>
2023-06-24 01:09:06 +0330

    Adding bootstrap and creating header component and register page

---
+++ components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navid.io</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <NuxtLink class="nav-link" activeClass="active"  to="/">Home</NuxtLink>
+                </li>
+                <li class="nav-item">
+                    <NuxtLink class="nav-link" activeClass="active"  to="/">Posts</NuxtLink>
+                </li>
+                <li class="nav-item">
+                    <NuxtLink class="nav-link" activeClass="active"  to="/">Profile</NuxtLink>
+                </li>
+                <li class="nav-item">
+                    <NuxtLink class="nav-link" activeClass="active"  to="/auth/register">Register</NuxtLink>
+                </li>
+                <li class="nav-item">
+                    <NuxtLink class="nav-link" activeClass="active"  to="/">Login</NuxtLink>
+                </li>
+                <li class="nav-item">
+                    <NuxtLink class="nav-link" activeClass="active"  to="/">Logout</NuxtLink>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            </div>
+        </div>
+        </nav>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-body text-center">
+                <h1 class="mb-4">NAVID.io</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, voluptas quo saepe iusto debitis recusandae laborum quasi deleniti eligendi laudantium a architecto cupiditate nisi suscipit, ab nemo consequuntur. Similique, aperiam.</p>
+            </div>
+        </div>
+    </div>
+</template>
