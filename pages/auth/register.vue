a3a1654ccc99ff54fb6bf2f90f6606bc6a1b852d
Navid Nahardani <nahardaninavid1993@gmail.com>
2023-06-24 01:43:56 +0330

    Adding input into register page and creating register js file in nuxt server and sending request into nuxt server for registeration

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,20 +1,51 @@
 <template>
-    <div>
-        <form>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <div class="">
+        <div class=" wrapper m-auto">
+            <form @submit.prevent="register()">
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name </label>
+                    <input type="text" class="form-control"  aria-describedby="emailHelp" id="name" v-model="formData.name">
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="text" class="form-control"  aria-describedby="emailHelp" id="email" v-model="formData.email">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="Password" class="form-label">Password</label>
+                    <input type="password" class="form-control"  aria-describedby="emailHelp" id="Password" v-model="formData.password">
+                </div>
+                <div class="mb-3">
+                    <label for="c_password" class="form-label">Confirm Password</label>
+                    <input type="password" class="form-control" id="c_password" v-model="formData.c_password">
+                </div>
+                <button type="submit" class="btn btn-primary">Register</button>
+            </form>
         </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
     </div>
 </template>
+<script setup>
+    const formData = reactive({
+        name: '',
+        email: '',
+        password: '',
+        c_password: '',
+    })
+    async function register(){
+        try {
+            const user = await $fetch('/api/auth/register',{
+                method: "POST",
+                body: formData,
+            })
+        } catch (error) {
+            console.log(error)
+        }
+        // console.log(formData)
+    }
+</script>
+<style scoped>
+    .wrapper{
+        width: 400px;
+        padding-top: 50px;
+    }
+</style>
